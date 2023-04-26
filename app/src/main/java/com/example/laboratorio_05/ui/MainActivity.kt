7b16cfe542993048444570a8058bec8cd316f6e5
davidperlera <misaelramirez123a@gmail.com>
2023-04-26 14:58:28 -0600

    Lab 06: añadiendo funcionalidad a newFragment

--- app/src/main/java/com/example/laboratorio_05/ui/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio_05/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.laboratorio_05
+package com.example.laboratorio_05.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.laboratorio_05.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
